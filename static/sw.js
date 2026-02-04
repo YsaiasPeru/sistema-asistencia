ef365768269e196ab66475aa71a3607a72c017f6
YSAIAS GODOY <ysaias.y.godoy.g@gmail.com>
2026-02-04 11:27:20 -0500

    PWA ready

---
+++ static/sw.js
@@ -0,0 +1,18 @@
+const CACHE = "asistencia-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/static/style.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
